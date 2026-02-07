7713832265ce46a0a2dc97885c7345f5bb1f4595
kelvinbosco2016-beep <kelvinbosco2016@gmail.com>
2026-02-07 21:34:15 +0300

    upload initial python project files

---
+++ shopping cart.py
@@ -0,0 +1,37 @@
+def add_items():
+ items = {}
+ items["item1"] = input("enter first item: ")
+ items["item2"] = input("enter second item: ")
+ print(items)
+print("items added: ")
+
+ 
+add_items()
+
+
+
+# showing items
+def show_item(items):
+ items={}
+ return items
+show_item('items')
+
+# price calculation
+def price_calc(price):
+ items={}
+ items['item1'] = int(input("enter price for item1: "))
+ items['item2'] = int(input("enter price for item2: "))
+ return price
+price_calc('price')
+
+# removal of items
+def remove_item(remove):
+ items={}
+ items['item1'] = input("remove item 1: ")
+ items['item1'] = input("remove item 1: ")
+ return remove
+
+remove_item('remove')
+print("items removed: ")
+# kelvin bosco 'SHOPPING CART CHALLENGE'
+
